c41b891884d6de39587059641fcd0406cc11573e
carmenflorsl <164540909+carmenflorsl@users.noreply.github.com>
2024-06-17 14:42:26 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#generadorNombres").innerHTML = generadorNombres();
+  });
+  console.log("Buenos dias!");
 };
+
+function generadorNombres() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".net", ".us", ".io"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let adjIndx = Math.floor(Math.random() * adj.length);
+  let nounIndx = Math.floor(Math.random() * noun.length);
+  let domIndx = Math.floor(Math.random() * dom.length);
+
+  return pronoun[proIndx] + adj[adjIndx] + noun[nounIndx] + dom[domIndx];
+}
